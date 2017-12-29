69ef641d94590e1acb4e12a43287803b5746db41
bculberson <brad.culberson@sendgrid.com>
2017-12-28 19:46:44 -0700

    set cache headers

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 npm run build
-pushd build
-s3cmd -P sync . s3://dogpointing
-popd
-aws cloudfront create-invalidation --distribution-id E39NAYF10CHP4U --paths / /index.html
+
+aws s3 cp ./build/static s3://dogpointing/static --recursive --include "*" --cache-control max-age=86400
+aws s3 cp ./build/favicon.ico s3://dogpointing/favicon.ico --cache-control max-age=3600
+aws s3 cp ./build/asset-manifest.json s3://dogpointing/asset-manifest.json --cache-control max-age=0
+aws s3 cp ./build/index.html s3://dogpointing/index.html --cache-control max-age=0
+aws s3 cp ./build/manifest.json s3://dogpointing/manifest.json --cache-control max-age=0
+aws s3 cp ./build/service-worker.js s3://dogpointing/service-worker.js --cache-control max-age=0
